60bc5240e56cafa28db3eec88f3be0304f9fc45b
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 20:54:49 -0300

    Projeto pré preparado em espera para add usuario

---
+++ src/main/java/com/lojadegames/lojadegamescomusuario/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.lojadegames.lojadegamescomusuario.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lojadegames.lojadegamescomusuario.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+	public List<Categoria> findAllByNomeContainingIgnoreCase (String nome);
+
+}
---
+++ src/test/java/com/lojagamesUsuario/loja/com/usuario/LojaComUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojagamesUsuario.loja.com.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaComUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7ee1321aad09e6803e4c62311e168cc710bcf065
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-02-24 21:47:35 -0300

    implementação dos metodos post, put e delete

--- src/main/java/com/lojadegames/lojadegamescomusuario/controller/UsuarioController.java
+++ src/main/java/com/lojadegames/lojadegamescomusuario/controller/UsuarioController.java
@@ -3,10 +3,15 @@ package com.lojadegames.lojadegamescomusuario.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,22 +22,37 @@ import com.lojadegames.lojadegamescomusuario.repository.UsuarioRepository;
 @RequestMapping("/usuario")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
-	
+
 	@Autowired
 	public UsuarioRepository repository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Usuario>> findAll (){
+	public ResponseEntity<List<Usuario>> findAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Usuario> findById (@PathVariable long id ){
+	public ResponseEntity<Usuario> findById(@PathVariable long id) {
 		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 
 	@GetMapping("/usuario/{usuario}")
-	public ResponseEntity<List<Usuario>> findByName (@PathVariable String nome){
+	public ResponseEntity<List<Usuario>> findByName(@PathVariable String nome) {
 		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+
+	@PostMapping
+	public ResponseEntity<Usuario> post(@RequestBody Usuario usuarios) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(usuarios));
+	}
+
+	@PutMapping
+	public ResponseEntity<Usuario> put(@RequestBody Usuario usuarios) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(usuarios));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(long id) {
+		repository.deleteById(id);
 	}
 }
